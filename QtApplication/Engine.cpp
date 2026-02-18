8f626201d273b21fc51b32b7afbcaee14327c118
seeroy667 <138810937+seeroy667@users.noreply.github.com>
2026-02-18 16:29:29 -0500

    Movement fixed

--- QtApplication/Engine.cpp
+++ QtApplication/Engine.cpp
@@ -37,10 +37,10 @@ void Engine::gameLoop()
     //float deltaTime = elapsedTimer.restart() / 1000.0f;
     //if (deltaTime > 0.1f) deltaTime = 0.1f; Will be usefull when treating deltaTime (gap in between frames)
 
-    if(cManager->movingRight()) p->setPosition(p->getPosition().x+0.5f, p->getPosition().y);
-    if(cManager->movingLeft()) p->setPosition(p->getPosition().x-0.5f, p->getPosition().y);
-    if(cManager->movingFront()) p->setPosition(p->getPosition().x, p->getPosition().y+0.5f);
-    if(cManager->movingBack()) p->setPosition(p->getPosition().x, p->getPosition().y-0.5f);
+    if(cManager->movingBack()) p->setPosition(p->getPosition().x+(0.5f*cos(p->getAngle() - M_PI/2)), p->getPosition().y+(0.5f*sin(p->getAngle() - M_PI/2)));
+    if(cManager->movingLeft()) p->setPosition(p->getPosition().x-(0.5f*cos(p->getAngle())), p->getPosition().y-(0.5f*sin(p->getAngle())));
+    if(cManager->movingRight()) p->setPosition(p->getPosition().x+(0.5f*cos(p->getAngle())), p->getPosition().y+(0.5f*sin(p->getAngle())));
+    if(cManager->movingFront()) p->setPosition(p->getPosition().x+(0.5f*cos(p->getAngle() + M_PI/2)), p->getPosition().y+(0.5f*sin(p->getAngle() + M_PI/2)));
     if(cManager->rotatingLeft()) p->setAngle(p->getAngle()+0.05f);
     if(cManager->rotatingRight()) p->setAngle(p->getAngle()-0.05f);
 
