d2bf4cc549904aef28d5e03fbbdbd7bd2d307f1f
seeroy667 <138810937+seeroy667@users.noreply.github.com>
2026-04-16 21:29:12 -0400

    Boss rendering fix

--- QtApplication/Engine.cpp
+++ QtApplication/Engine.cpp
@@ -281,14 +281,6 @@ void Engine::handleShooting()
 
 void Engine::handleRendering(float deltaTime)
 {
-    if(gManager->isBossRenderable())
-    {
-        if(gManager->getBSP()->enemyRendering(gManager->getPlayer()->getPosition(), gManager->getBoss()->getPosition(), gManager->getVerteces()))
-        {
-            rManager->renderActor(gManager->getBoss(),*gManager->getPlayer(),QColor(150,0,0), 2.5f);
-        }
-    }
-
     rManager->render(*gManager->getPlayer(),
                      gManager->getRenderedEnemy(),
                      gManager->getRenderedRangedEnemies(),
@@ -298,6 +290,14 @@ void Engine::handleRendering(float deltaTime)
                      gManager->getBSP(),
                      gManager->getVerteces(),
                      gManager->getSectors());
+
+    if(gManager->isBossRenderable())
+    {
+        if(gManager->getBSP()->enemyRendering(gManager->getPlayer()->getPosition(), gManager->getBoss()->getPosition(), gManager->getVerteces()))
+        {
+            rManager->renderActor(gManager->getBoss(),*gManager->getPlayer(),QColor(150,0,0), 2.5f);
+        }
+    }
 }
 
 /*
